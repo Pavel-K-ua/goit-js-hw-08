36703f64e17da92e93c005d755668f1f38bb3eae
Pavel-K-ua <pavel.kolosyuk@gmail.com>
2023-08-14 15:35:21 +0300

    add 1st

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+    galleryLIst: document.querySelector(".gallery"),
+  };
+  
+  const markup = galleryItems
+    .map((item) => {
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`;
+    })
+    .join("");
+  
+  refs.galleryLIst.insertAdjacentHTML("afterbegin", markup);
+  
+  var lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
