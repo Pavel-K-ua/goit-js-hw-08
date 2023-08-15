0596db7c923c2f08a3a73f3c76dd43518b593d80
Pavel-K-ua <pavel.kolosyuk@gmail.com>
2023-08-15 12:55:49 +0300

    2nd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', function({seconds}) {
+localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
+});
+
+player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")));
